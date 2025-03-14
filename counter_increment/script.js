f8779371448728ce3e3aacfe00ef39dda43ffb68
chiro <chirochiro016@gmial.com>
2025-03-14 15:52:57 +0900

    リセットボタン追加

--- counter_increment/script.js
+++ counter_increment/script.js
@@ -1,14 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const counterInc = document.getElementById("counter-inc-btn")
     const counterContainer = document.getElementsByClassName("counter-container")[0];
+    let counterId = 0;
+
+    const counterInc = document.getElementById("counter-inc-btn")
     counterInc.addEventListener("click", function(){
         createCounter();
     })
 
+    const counterSub = document.getElementById("counter-sub-btn")
+    counterSub.addEventListener("click", function(){
+        removeCounter();
+    })
+
     function createCounter(){
+        counterId++;
 
         const counterWrapper = document.createElement("div");
         counterWrapper.classList.add("counter-wrapper")
+        counterWrapper.id = `counter-${counterId}`;
 
         const counterDisplay = document.createElement("p");
         counterDisplay.textContent = "0"
@@ -33,5 +42,11 @@ document.addEventListener("DOMContentLoaded", function(){
         counterWrapper.appendChild(counterButton);
         counterWrapper.appendChild(resetButton);
         counterContainer.appendChild(counterWrapper);
+        }
+
+        function removeCounter(){
+            const counterWrapper = document.getElementById(`counter-${counterId}`);
+            counterWrapper.remove();
+            counterId--;
         }
 })
