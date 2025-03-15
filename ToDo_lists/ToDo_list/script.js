6d6fb7d5917ef94078a0509147e8af5c24f2752c
chiro <chirochiro016@gmial.com>
2025-03-15 22:25:08 +0900

    ローカルストレージ活用

--- ToDo_lists/ToDo_list/script.js
+++ ToDo_lists/ToDo_list/script.js
@@ -1,17 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    
     const taskContainer = document.getElementById("task-container");
     const addTaskButton = document.getElementById("add-task-button");
     addTaskButton.addEventListener("click", function(){
         addTask();
     })
 
-    function addTask(){
+    console.log("localstorage", localStorage.getItem("tasks"))
+    console.log("tasks", tasks)
+    tasks.forEach(element => {
+        addTask(element)
+    });
+
+    function addTask(savedTask = ""){
         const taskWrapper = document.createElement("li");
         taskWrapper.classList.add("task-wrapper");
 
+        const checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+
         const taskInput = document.getElementById("task-input");
         const taskText = document.createElement("span");
-        taskText.textContent = taskInput.value;
+        taskText.classList.add("task-text")
+        taskText.textContent = taskInput.value + savedTask;
         if(taskText.textContent === "") return;
         
         const deleteButton = document.createElement("button");
@@ -19,13 +31,23 @@ document.addEventListener("DOMContentLoaded", function(){
         deleteButton.textContent = "削除";
         deleteButton.addEventListener("click", function(){
             taskWrapper.remove();
+            updateLocalStorage();
         })
 
+        taskWrapper.appendChild(checkBox);
         taskWrapper.appendChild(taskText);
         taskWrapper.appendChild(deleteButton);
         taskContainer.appendChild(taskWrapper);
+        updateLocalStorage();
 
         taskInput.value = "";
         console.log("task added");
+    }
+
+    function updateLocalStorage(){
+        let tasks = Array.from(document.getElementsByClassName("task-wrapper")).map(element =>
+            element.querySelector(".task-text").textContent
+        )
+        localStorage.setItem("tasks", JSON.stringify(tasks));
     }
 })
